655ce98b6204f99ac69d3cc8f460d2631d3709d0
rahafbakeer <rbakeer@i3hub.com>
2022-08-09 23:19:52 +0300

    commit

--- src/pages/Editpage.tsx
+++ src/pages/Editpage.tsx
@@ -1,26 +1,38 @@
 import React from "react";
 
 import { useTable } from "react-table";
-import axios from 'axios';
+import axios from "axios";
 class Editclass extends React.Component<any, any> {
   state = {
     data: [],
     error: null,
     stu_id: null,
-    canIdet:null
+    canIdet: null,
+    message: null,
   };
-   componentDidMount=async()=> {
+  componentDidMount = async () => {
     try {
       const response = await axios.get(
         `http://localhost:1337/api/students/${this.state.stu_id}`
       ); //
       // const data = await response.json();
 
-      this.setState({ data: response.data.data });
+      this.setState({ data: response?.data?.data });
     } catch (error) {
       this.setState({ error });
     }
-  }
+  };
+
+  handleChange = (event) => {
+    this.setState({ message: event.target.value });
+    console.log("value........", event.target.value);
+  };
+  handleClick = (event) => {
+    event.preventDefault();
+
+    // 👇️ value of input field
+    console.log("Click.............", this.state.message);
+  };
 
   renderInputEdit() {
     const row_data = [
@@ -30,83 +42,57 @@ class Editclass extends React.Component<any, any> {
         isHidden: false,
       },
     ];
-    let dataArrObj = []
- dataArrObj=Object.values(this.state.data? this.state.data:[{}]);
-// dataArrObj=Object.entries(dataArrObj[0]? dataArrObj[0]:[{}]);
-let objdata=dataArrObj[1];
-// console.log(Object.keys(objdata))
+    let dataArrObj = [];
+    dataArrObj = Object.values(this.state.data ? this.state.data : [{}]);
 
+    dataArrObj = Object.entries(dataArrObj[1] ? dataArrObj[1] : [{}]);
 
-console.log(dataArrObj)
-console.log("dfs2222222222222sssssssss")
- dataArrObj=Object.entries(dataArrObj[1]? dataArrObj[1]:[{}]);
- console.log(dataArrObj)
- dataArrObj=Object.values(dataArrObj).filter(function (f) {
-  return (
-
-    f [0]!== "createdAt" &&
-    f[0] !== "updatedAt" &&
-    f[0] !== "publishedAt" &&
-    f[0] !== "locale" &&
-    f[0] !== "localizations"
-  );
-});
-
-
-console.log(dataArrObj)
-
-return dataArrObj.map((data: any, index) => {
-    console.log(data)
-  //  let keysofData=Object.keys(data).filter(function (f) {
-  //   return (
-  //     f !== "createdAt" &&
-  //     f !== "updatedAt" &&
-  //     f !== "publishedAt" &&
-  //     f !== "locale" &&
-  //     f !== "localizations"
-  //   );
-  // });
-  console.log("ssgeeeeeeeeeeeeeeeeeeeeeeeeeeeeeehhhhhhhhhhhhhssssssssssssssssssssssssssssssss")
-
-  console.log(data)
-let keysofData=data.map((key:any) =>{
-  console.log(key[0])
-  return  key[0]
-})
-console.log("sfdszassssshhhhhhhhhhhhhhhhhhssssssssssssssssssssssssssssssss")
-  console.log(keysofData)
-
-  // for (let i = 0; i < keysofData.length; i++) {
-  //   row_data.push({
-  //     key: keysofData[i],
-  //     display: data?.attributes[keysofData[i]],
-  //     isHidden: true,
-  //   });
-  // }
+    dataArrObj = Object.values(dataArrObj)?.filter(function (f) {
       return (
-
-      <input type="text" name="name" />
-
+        f[0] !== "createdAt" &&
+        f[0] !== "updatedAt" &&
+        f[0] !== "publishedAt" &&
+        f[0] !== "locale" &&
+        f[0] !== "localizations"
       );
-      
-      
-      
-     
+    });
+
+    return dataArrObj?.map((data: any, index) => {
+      let keysofData = data[0];
+
+      row_data.push({
+        key: keysofData,
+        display: data[1],
+
+        isHidden: true,
+      });
+
+      if (row_data[0].key === "") {
+        row_data.shift();
+      }
+
+      return (
+        <input
+          type="text"
+          name="name"
+          onChange={this.handleChange}
+          value={this.state.message?this.state.message:""}
+        />
+      );
     });
   }
   render() {
-    console.log(this.state.data);
-    console.log(window.location.href);
-    const linkId = window.location.href.split("/");
+    const linkId = window?.location?.href.split("/");
     this.state.stu_id = linkId.at(-1);
-    console.log(linkId.at(-1));
-    console.log(`http://localhost:1337/api/students/${this.state.stu_id}`);
 
     return (
       <div>
         <br></br>
         <br></br>
         {this.renderInputEdit()}
+        <button className="ta-button" onClick={this.handleClick}>
+          ADD
+        </button>
       </div>
     );
   }
